0d26ac0c8926469d63d12d635cca9d6311e7bd94
Ravi <ravikumarsinha234@gmail.com>
2023-11-11 00:42:04 +0530

    commiting trials and data ingestion basic

--- src/kidneyDiseaseClassifier/utils/common.py
+++ src/kidneyDiseaseClassifier/utils/common.py
@@ -1,14 +1,13 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from kidneyDiseaseClassifier import logger
+from src.kidneyDiseaseClassifier import logger
 import json
 import joblib
 from ensure import ensure_annotations
 from box import ConfigBox
 from pathlib import Path
 from typing import Any
-import base
 
 
 @ensure_annotations
